0fd0966c7777aae622fff45d574374875aedf29f
王智宇 <wangzhiyu@shopex.cn>
2016-04-18 23:12:57 +0800

    angularjs version exchange

--- code/002-控制器/index.js
+++ code/002-控制器/index.js
@@ -2,10 +2,10 @@
  * Created by evelyncyrus on 2016/3/21.
  */
 
-var firstController=function($scope){
-    // $scope 作用域
-    // 申明一个MODEL
-    $scope.name = '张三';
-    $scope.age  = '20';
+var firstController = function ($scope) {
+  // $scope 作用域
+  // 申明一个MODEL
+  $scope.name = '张三';
+  $scope.age = '20';
 
 }
